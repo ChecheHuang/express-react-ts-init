1ba6fb2026436dd606097df278643a541256e155
Carl <qwerfcxzas1@gmail.com>
2022-10-27 23:41:37 +0800

    modify

--- client/src/redux/hooks.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useAppDispatch = exports.useAppSelector = void 0;
-const react_redux_1 = require("react-redux");
-exports.useAppSelector = react_redux_1.useSelector;
-const useAppDispatch = () => (0, react_redux_1.useDispatch)();
-exports.useAppDispatch = useAppDispatch;
